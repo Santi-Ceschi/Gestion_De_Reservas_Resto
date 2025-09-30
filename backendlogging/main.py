449596eae0b3b83a542e1adef33fc3ee06e01466
Juan Pablo Ceschi <juanc@vertiq.com>
2025-09-29 22:05:51 -0300

    agregado cors

--- backendlogging/main.py
+++ backendlogging/main.py
@@ -1,11 +1,27 @@
 from fastapi import FastAPI
 from database import Base, engine
 from routers import users
+from fastapi.middleware.cors import CORSMiddleware
 
 # Crear tablas si no existen
 Base.metadata.create_all(bind=engine)
 
 app = FastAPI(title="Sistema de Reservas - LaForja")
+
+origins = [
+    # Permite el acceso desde el mismo origen de la documentación
+    "http://127.0.0.1:8000",
+    "http://localhost:8000",
+    # Puedes añadir otros dominios o puertos que uses, si es necesario
+]
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,                     # Lista de orígenes permitidos
+    allow_credentials=True,                    # Permite cookies de origen cruzado (si aplica)
+    allow_methods=["*"],                       # Permite todos los métodos (GET, POST, etc.)
+    allow_headers=["*"],                       # Permite todos los encabezados
+)
 
 # Rutas
 app.include_router(users.router)
