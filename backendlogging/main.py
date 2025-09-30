e83aeee37b190a72ebc29e3b00598da61aeecad1
Santi-Ceschi <santiagoceschi126@gmail.com>
2025-09-30 10:26:13 -0300

    RegistrarUsuario
    agrego .py para registrar un usuario, y se modificaron versiones en requirements

--- backendlogging/main.py
+++ backendlogging/main.py
@@ -1,15 +1,33 @@
 from fastapi import FastAPI
 from database import Base, engine
 from routers import users
+from fastapi.middleware.cors import CORSMiddleware
+from routers.register import router as register_router
 
 # Crear tablas si no existen
 Base.metadata.create_all(bind=engine)
 
 app = FastAPI(title="Sistema de Reservas - LaForja")
 
+origins = [
+    # Permite el acceso desde el mismo origen de la documentación
+    "http://127.0.0.1:8000",
+    "http://localhost:8000",
+    # Puedes añadir otros dominios o puertos que uses, si es necesario
+]
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,                     # Lista de orígenes permitidos
+    allow_credentials=True,                    # Permite cookies de origen cruzado (si aplica)
+    allow_methods=["*"],                       # Permite todos los métodos (GET, POST, etc.)
+    allow_headers=["*"],                       # Permite todos los encabezados
+)
+
+
 # Rutas
 app.include_router(users.router)
-
+app.include_router(register_router)
 @app.get("/")
 def root():
     return {"mensaje": "API LaForja funcionando 🚀"}
